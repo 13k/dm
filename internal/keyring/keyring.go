61abc24fb90052bd2afe716b7a0c0c225e2e981e
13k <kbmurata@gmail.com>
2022-09-13 12:04:59 -0300

    refactor(keyring): update code to use `99designs/keyring`

--- internal/keyring/keyring.go
+++ internal/keyring/keyring.go
@@ -1,15 +1,56 @@
 package keyring
 
 import (
-	"github.com/zalando/go-keyring"
+	"fmt"
+
+	"github.com/99designs/keyring"
 
 	"github.com/13k/dm/pkg/meta"
 )
 
+func open() (keyring.Keyring, error) {
+	ring, err := keyring.Open(keyring.Config{
+		ServiceName: meta.AppID,
+	})
+
+	if err != nil {
+		err = fmt.Errorf("could not open keyring for service %q: %w", meta.AppID, err)
+	}
+
+	return ring, err
+}
+
 func Get(key string) (string, error) {
-	return keyring.Get(meta.AppID, key)
+	ring, err := open()
+
+	if err != nil {
+		return "", err
+	}
+
+	item, err := ring.Get(key)
+
+	if err != nil {
+		return "", fmt.Errorf("could not get keyring item for key %q: %w", key, err)
+	}
+
+	return string(item.Data), nil
 }
 
 func Set(key, value string) error {
-	return keyring.Set(meta.AppID, key, value)
+	ring, err := open()
+
+	if err != nil {
+		return err
+	}
+
+	item := keyring.Item{
+		Key:  key,
+		Data: []byte(value),
+	}
+
+	if err := ring.Set(item); err != nil {
+		return fmt.Errorf("could not set keyring item for key %q: %w", key, err)
+	}
+
+	return nil
 }
